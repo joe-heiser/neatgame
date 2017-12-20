f3503f6558a1769e54b8580e95996fbec1fca5d6
LukeDesktop <lcetnar123@gmail.com>
2017-12-20 03:08:47 -0500

    d

--- neatgame/src/neatgame/Enemy.java
+++ neatgame/src/neatgame/Enemy.java
@@ -2,20 +2,20 @@ package neatgame;
 
 import java.util.Random;
 
-public class enemy{
+public class Enemy{
 private String name;
 private int health;
 private int damage;
 private int accuracy;
 
-public enemy(String name, int damage, int health) {
+public Enemy(String name, int damage, int health) {
 	this.name = name;
 	this.health = health;
 	this.damage = damage;
 	this.accuracy = 100;
 }
 
-public enemy(String name, int damage) {
+public Enemy(String name, int damage) {
 	this.name = name;
 	this.health = 100;
 	this.damage = damage;
