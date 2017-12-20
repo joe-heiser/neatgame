86fa341f307b2ae4ed254dcce5e6bbd41efde4b5
LukeDesktop <lcetnar123@gmail.com>
2017-12-20 02:49:50 -0500

    kj

--- neatgame/src/neatgame/game.java
+++ neatgame/src/neatgame/game.java
@@ -5,7 +5,7 @@ public class game {
 	public static void main(String[] args)
 	{
 	 Player p1 =  new Player("Jeffy");
-	 p1.TakeDamage(100);
+	 p1.takeDamage(100);
 	 System.out.print(p1.toString());
 	 
 	}
