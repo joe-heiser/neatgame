beeadd834cab0c1be78a38646c103cf433575c08
joe-heiser <34702912+joe-heiser@users.noreply.github.com>
2017-12-20 02:45:37 -0500

    Gui Test

---
+++ neatgame/src/neatgame/Gui.java
@@ -0,0 +1,64 @@
+package neatgame;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractButton;
+import javax.swing.GroupLayout;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class Gui extends JFrame {
+	
+	public Gui() {
+
+        initUI();
+    }
+
+    private void initUI() {
+    	
+    	Player p1 =  new Player("Jeffy",50,10);
+    	
+    	JFrame frame = new JFrame("test");
+    	
+    	JPanel panel = new JPanel(new GridLayout(4,4,4,4));
+    	
+    	JButton healButton = new JButton("Heal");
+    	healButton.setPreferredSize(new Dimension(50,50));
+    	
+    	JLabel health = new JLabel(p1.toString());
+    	
+    	
+    	
+    	healButton.addActionListener((ActionEvent event) -> {
+            p1.heal();
+            health.setText(p1.toString());
+        });
+    	
+    	
+    	panel.add(healButton);
+    	panel.add(health);
+    	frame.setContentPane(panel);
+    	frame.pack();
+    	frame.setVisible(true);
+    	       
+        setTitle("Neat Game");
+        setSize(800, 600);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(() -> {
+            Gui ex = new Gui();
+            ex.setVisible(true);
+        });
+    }
+
+}
